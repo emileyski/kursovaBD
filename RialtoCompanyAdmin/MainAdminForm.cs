df0d496ea46b024c5ccc5af04f07c2cab253c80d
emileyski <bohdan.ilienko@nure.ua>
2023-02-10 18:52:16 +0200

    4

--- RialtoCompanyAdmin/MainAdminForm.cs
+++ RialtoCompanyAdmin/MainAdminForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RialtoCompanyAdmin
 {
-    public partial class Form1 : Form
+    public partial class MainAdminForm : Form
     {
-        public Form1()
+        public MainAdminForm()
         {
             InitializeComponent();
         }
--- RialtoCompanyAdmin/Program.cs
+++ RialtoCompanyAdmin/Program.cs
@@ -16,7 +16,7 @@ namespace RialtoCompanyAdmin
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainAdminForm());
         }
     }
 }
